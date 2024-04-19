0a86443ff1bbda0ef6d8f0fe1502009b85483646
Mohammed Sameer <116733116+S-a-m-e-e-r@users.noreply.github.com>
2024-04-19 09:48:07 +0530

    Add files via upload

---
+++ portfolio/main.js
@@ -0,0 +1,8 @@
+
+var typed = new Typed('.auto-text', {
+  strings: ['a Student.' , 'a Coder.' , 'an Explorer.' , 'a Developer.'],
+  typeSpeed:100,
+  backSpeed:100,
+  backDelay:1000,
+  loop:true
+});
